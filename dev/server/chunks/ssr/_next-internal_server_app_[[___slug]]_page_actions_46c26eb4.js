4d2a75dac1e869eea5222d6e1e00f0ad8a580369
tomer <paz.tomer@gmail.com>
2026-02-26 07:56:21 +1030

    Updates

--- dev/server/chunks/ssr/_next-internal_server_app_[[___slug]]_page_actions_46c26eb4.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/[[...slug]]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%5B%5B___slug%5D%5D_page_actions_46c26eb4.js.map
